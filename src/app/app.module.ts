53b5d242f80fbcc1f5d1720371360425ea3b88c4
DgtalBug <dgtalbug@air.local>
2020-05-10 14:14:49 +0530

    Added Basic Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ArticleListrComponent } from './article-listr/article-listr.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    ArticleListrComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/article-listr/article-listr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-listr',
+  templateUrl: './article-listr.component.html',
+  styleUrls: ['./article-listr.component.css']
+})
+export class ArticleListrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
